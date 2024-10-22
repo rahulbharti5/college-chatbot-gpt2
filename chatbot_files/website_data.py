666f9be8fc9bd8f5c15ffdcbde4e49a5d8e42b1f
Ubuntu <rahulbharti@Ubuntu20.q3p5cfi15ayuhn1poex5rdn01b.xx.internal.cloudapp.net>
2024-10-22 08:18:42 +0000

    Inital Commit

---
+++ chatbot_files/website_data.py
@@ -0,0 +1,218 @@
+
+## Factod Data Conversation
+about_college = [
+  [
+    "When was Rajkiya Engineering College (R.E.C.) Ambedkar Nagar established?",
+    "Rajkiya Engineering College (R.E.C.) Ambedkar Nagar was established in 2010 by the Government of Uttar Pradesh under a special component plan.",
+    "What were the initial branches offered by the college?",
+    "The college initially offered B.Tech. programs in three branches: Information Technology (IT), Electrical Engineering (EE), and Civil Engineering (CE), with an intake of 60 students in each branch.",
+    "How long did it take for the college to shift to its own campus?",
+    "The college initially operated from the campus of Kamla Nehru Institute of Technology (K.N.I.T) in Sultanpur. It shifted to its own campus in Ambedkar Nagar in August 2012, about two years after its establishment."
+  ],
+  [
+    "What is the budget allocated for the construction of the college?",
+    "The college construction started in October 2010 with an allocated budget of 6213.81 Lakh.",
+    "Who served as the Principal of Rajkiya Engineering College (R.E.C.) Ambedkar Nagar before January 2015?",
+    "The Director of K.N.I.T Sultanpur served as the Principal of Rajkiya Engineering College (R.E.C.) Ambedkar Nagar until January 14, 2015."
+  ],
+  [
+    "Is Rajkiya Engineering College (R.E.C.) Ambedkar Nagar affiliated with any university?",
+    "Yes, the college is a constituent college of Uttar Pradesh Technical University, Lucknow.",
+    "Who is the current Director of Rajkiya Engineering College (R.E.C.) Ambedkar Nagar?",
+    "Professor K.S. Verma is the current Director (Founder) of Rajkiya Engineering College (R.E.C.) Ambedkar Nagar."
+    "What are the central Central Facilties",
+    "Canteen,Boy’s Hostel,Girl’s Hostel, ATM & Banks, Library, Computer Center, Community Hall (Seminar), Play Ground",
+    "How to reach ?",
+    "The Institute is situated 3 Km away from the city(Akbarpur Bus Stand) on Tanda road near hawaipatti. It is well connected through road and rail network. The nearest airport is Babatpur(Varanasi) which is about 100 Km far from the Institute."
+  ]
+]
+
+about_it_department = [
+  [
+    "When was the Department of Information Technology established at Rajkiya Engineering College (R.E.C.) Ambedkar Nagar?",
+    "The Department of Information Technology was established in 2010, the same year the college was founded.",
+    "What is the initial intake capacity of the IT department?",
+    "The department started with an intake of 60 students. there are some other 6 seats for Lateral Entry Students.",
+    "What are some of the key strengths of the IT department at REC Ambedkar Nagar?"
+    "The department boasts highly qualified and experienced faculty members with varied specializations. They are actively involved in research and development, publishing papers and presenting at conferences. The department also provides state-of-the-art computer facilities and focuses on practical learning through real-life projects."
+  ],
+  [
+    "What are the key objectives of the IT (Information Technlogy) department?",
+    "The IT department aims to become a center of excellence in technical higher education, research, and support services. Their goal is to contribute significantly to individual and societal empowerment.",
+    "What is the vision of the IT department?",
+    "The IT department envisions becoming a center of excellence in information technology, producing globally competent engineers equipped with strong technical skills and ethical values. They aim to contribute to the betterment of society."
+  ],
+  [
+    "What are the key missions of the IT department?",
+    "The IT department's missions are to groom students with leadership and communication skills through real-life project work, create an environment for engineering knowledge and innovative research." 
+  ],
+   [
+    "Who is the HOD of the Faculty of Information Technology?",
+    "Dr. Sudhakar Tripathi is the Associate Professor (Regular) & HOD of the Faculty of Information Technology.",
+    "What are the qualifications of Mr. Shivendu Mishra?",
+    "Mr. Shivendu Mishra holds a B.Tech and M.Tech degree from MNNIT Allahabad.",
+    "What is Dr. Ramesh Chand Pandey's area of interest?",
+    "Dr. Ramesh Chand Pandey's research interests lie in Image Processing, Video Processing, Pattern Recognition, Machine Learning, and IoT.",
+    "Can you list all the faculty members in the Faculty of Information Technology, along with their email addresses?",
+    "Sure! Here are the faculty members in the Faculty of Information Technology with their email addresses:\n\n- **Dr. Sudhakar Tripathi**: stripathi@recabn.ac.in\n- **Mr. Shivendu Mishra**: shivendu@recabn.ac.in\n- **Dr. Ramesh Chand Pandey**: rcpandey@recabn.ac.in\n- **Mr. Sharad Verma**: sharad@recabn.ac.in\n- **Dr. Ashish Kumar Mishra**: akmishra@recabn.ac.in\n- **Mr. Shivendra Kumar Pandey**: shivendra@recabn.ac.in\n- **Dr. Prince Rajpoot**: princeraj@recabn.ac.in\n- **Dr. Amit Kumar**: amitkumar@recabn.ac.in"
+  ],
+  [
+    "What is the email address of Dr. Ashish Kumar Mishra?",
+    "Dr. Ashish Kumar Mishra's email address is akmishra@recabn.ac.in.",
+    "What is the research publication record of Mr. Sharad Verma?",
+    "Mr. Sharad Verma has published 3 papers in international conferences, with one of them being Scopus indexed."
+  ],
+  [
+    "What is Dr. Prince Rajpoot's area of interest?",
+    "Dr. Prince Rajpoot's research interests lie in IoT, AI, ML, and Optimization in Wireless Sensor Networks using MADM and MODM approaches."
+  ]
+]
+
+about_ee_department = [
+  [
+    "What are the areas of focus within the Electrical Engineering department at REC Ambedkar Nagar?",
+    "The department focuses on providing a strong foundation in various areas of electrical engineering, including communication technology, control technology, electronics, and power & energy.",
+    "What opportunities are available for students to explore specific interests in Electrical Engineering?",
+    "The department offers elective courses carefully designed to cater to the interests of students, providing them with exposure to state-of-the-art technologies.",
+    "What is the initial intake capacity of the EE ( Electrical Engineering ) department?",
+    "The department started with an intake of 60 students. there are some other 6 seats for Lateral Entry Students.",
+  ],
+  [
+    "What are the key objectives of the EE department?",
+    "The department aims to become a center of excellence in technical higher education, research, and support services, contributing to individual and societal empowerment.",
+    "What is the vision of the EE department?",
+    "The EE department envisions producing globally competent engineers, innovators, and entrepreneurs who are instilled with strong human values and professional ethics."
+  ],
+  [
+    "What are the key missions of the EE department?",
+    "The EE department's missions are to deliver high-quality education and research in electrical engineering, provide knowledge base and consultancy services to rural and underprivileged communities for their development, and bridge the gap between industry and academia by aligning curricula and syllabi with industrial and societal needs."
+  ],
+    [
+    "Who is the head of the Faculty of Electrical Engineering?",
+    "Dr. S. P. Singh is the Associate Professor (Regular) & Head of the Faculty of Electrical Engineering.",
+    "What are the qualifications of Dr. Mohammed Aslam Husain?",
+    "Dr. Mohammed Aslam Husain holds a Ph.D. from AMU, an M.Tech. from AMU, and a B.Tech. from AMU.",
+    "Can you list all the faculty members in the Faculty of Electrical Engineering, along with their email addresses?",
+    "Here are the faculty members in the Faculty of Electrical Engineering with their email addresses:\n\n- **Dr. S. P. Singh**: drspsingh@recabn.ac.in\n- **Dr. Mohammed Aslam Husain**: mahusain@recabn.ac.in\n- **Mr. Vikas Patel**: vikaspatel@recabn.ac.in\n- **Dr. Puneet Joshi**: drpuneetj@recabn.ac.in\n- **Dr. Sanjay Agrawal**: sanjay@recabn.ac.in\n- **Dr. Yudhishthir Pandey**: yudhishthir@recabn.ac.in\n- **Dr. Lokesh Kumar Yadav**: lokeshky@recabn.ac.in\n- **Dr. Arif Iqbal**: arif.iqbal@recabn.ac.in\n- **Mr. Sonu Kumar**: sonu@recabn.ac.in" 
+  ],
+  [
+    "What is Dr. Sanjay Agrawal's area of interest?",
+    "Dr. Sanjay Agrawal's research interests lie in Power System Protection, Health Monitoring, Renewable Energy, and Smart Grid.",
+    "What is the email address of Mr. Sonu Kumar?",
+    "Mr. Sonu Kumar's email address is sonu@recabn.ac.in."
+  ],
+  [
+    "What is the experience of Dr. Yudhishthir Pandey?",
+    "Dr. Yudhishthir Pandey has 17 years of experience in the field of Electrical Engineering."
+  ],
+  [
+    "What are the research interests of Mr. Vikas Patel?",
+    "Mr. Vikas Patel's areas of interest include Power Electronics, Electrical Machines, and Renewable Energy."
+  ]
+]
+
+about_ce_department = [
+  [
+    "What is the primary focus of the Civil Engineering department at REC Ambedkar Nagar?",
+    "The Civil Engineering department focuses on the theory and practice of civil engineering, specifically in the construction of buildings, structures, roads, bridges, aqueducts, canals, ports, docks, and harbors.",
+     "What is the initial intake capacity of the CE ( Civil Engineering ) department?",
+    "The department started with an intake of 60 students. there are some other 6 seats for Lateral Entry Students.",
+    "What are the department's goals for its graduates?"
+    "The Civil Engineering department aims to produce high-quality technical manpower for industries, research and development organizations, academic institutions, and ultimately to meet the engineering needs of the nation."
+  ],
+  [
+    "What are the benefits of the extra-curricular and co-curricular activities encouraged by the Civil Engineering department?",
+    "These activities help students develop their personality, nurture teamwork, enhance organizational skills, and instill strong moral values and a positive work ethic."
+  ],
+  [
+    "What is the vision of the Civil Engineering department?",
+    "The department envisions providing quality education and skills to meet the challenges in the field of civil engineering and contribute to nation building.",
+    "What are the key missions of the Civil Engineering department?",
+    "The department's missions are to produce globally competitive civil engineering graduates by providing a conducive learning environment, to become a center of excellence in civil engineering and allied research for societal benefit, and to promote quality education through innovation, research, and consultancy projects for industrial and societal needs."
+  ],
+  [
+    "Who is the head of the Faculty of Civil Engineering?",
+    "Mr. Amit Kumar Rai is the Assistant Professor (Regular) & HOD of the Faculty of Civil Engineering.",
+    "What is the area of interest of Mr. Avaneesh Kumar Yadav?",
+    "Mr. Avaneesh Kumar Yadav's areas of interest include Water and Wastewater treatment and Solid Waste Management.",
+    "Can you list all the faculty members in the Faculty of Civil Engineering along with their email addresses?",
+    "Here are the faculty members in the Faculty of Civil Engineering with their email addresses:\n\n- **Dr. Ayush Mittal**: amittal@recabn.ac.in\n- **Mr. Avaneesh Kumar Yadav**: avaneesh@recabn.ac.in\n- **Mr. Amit Kumar Rai**: amitkrrai@recabn.ac.in\n- **Mr. Nitin Kumar Shukla**: nitin@recabn.ac.in"
+  ],
+  [
+    "What is the email address of Mr. Nitin Kumar Shukla?",
+    "Mr. Nitin Kumar Shukla's email address is nitin@recabn.ac.in."
+  ]
+]
+
+placement_overview = [
+  [
+    "Who heads the Training & Placement Cell at Rajkiya Engineering College (R.E.C.) Ambedkar Nagar?",
+    "The Training & Placement Cell is headed by Dr. Sanjay Agarwal, Assistant Professor in the Electrical Department.",
+    "What kind of support does the TPO provide for placements at REC Ambedkar Nagar?",
+    "The TPO, managed by an efficient team of office staff, handles all aspects of placements at the college, providing support to both students and recruiters."
+    "Who is current TPO Traning Placement Officer?",
+    "The Training & Placement Cell is headed by Dr. Sanjay Agarwal, Assistant Professor in the Electrical Department.",
+  ],
+  [
+    "What are the key characteristics that recruiters frequently cite about REC Ambedkar Nagar graduates?",
+    "Recruiters praise our students for their quick learning ability, versatility, teamwork skills, excellent analytical skills, and most importantly, their positive attitude towards work."
+  ],
+  [
+    "What are the goals of Rajkiya Engineering College (R.E.C.) Ambedkar Nagar in terms of student development?",
+    "The institute aims to produce professionally competent engineers by providing value-based and quality education, ensuring students are adaptable to the changing demands of the world."
+  ],
+  [
+    "What kind of facilities does REC Ambedkar Nagar provide for campus recruiters?",
+    "The college has excellent facilities for campus recruiters, and they are happy to make these facilities available to participating organizations.",
+    "What is the role of training and internship in the curriculum at REC Ambedkar Nagar?"
+    "Training and internship are integral parts of the curriculum. Pre-final B.Tech students can participate in training during their sixth semester in June-July, gaining practical knowledge of real-world applications."
+  ],
+  [
+    "What is the long-term vision for REC Ambedkar Nagar?",
+    "The institute's vision is to provide quality technical education, act as a platform for scientific research, and develop human potential to its fullest. They are striving to restore REC's position as a top technological institute in Uttar Pradesh.",
+  ],
+]
+
+hostel =[
+  [
+    "How many hostels are there in total for students?",
+    "There are a total of 8 hostels for students, but one is currently under construction.",
+    "What are the different types of rooms available for students?",
+    "Only Final Year (4rth Year) Year Students and some 3rd Year students of DDU Hostel Get single Room, and rest are three seaters including all girls also.",
+    "What kind of security measures are in place at the campus?",
+    "The campus has a security gate with staff controlling entry, making it a secure environment."
+  ],
+  [
+    "Can guests stay in the hostel?",
+    "Guests are allowed to stay in the hostel with prior permission from the warden. They must register their arrival in the visitor's register and inform the warden the next morning if permission was not obtained in advance.",
+    "Are there any restrictions on guests staying in the hostels?",
+    "Female guests are not allowed to stay in the Boys Hostel, and male guests are not allowed to stay in the Girls Hostel."
+  ],
+  [
+    "What are the rules regarding electrical appliances?",
+    "Students are not allowed to use electrical appliances like heaters or toasters without written permission from the warden.",
+    "What amenities are available for students?",
+    "The hostels offer furnished rooms with curtains, mattresses, fans, and wardrobes. Other amenities include a mess and dining hall, internet facility (Wi-Fi), solar water heaters, a gymnasium, TV room, library, table tennis, carrom, chess, pool table, football, volleyball, basketball, badminton, and a 24-hour generator facility."
+  ]
+]
+
+co_curricular =[
+  [
+    "What are some of the co-curricular activities offered at REC Ambedkar Nagar?",
+    "Students participate in various fests, including 'AVIGHNA', an annual college festival, and 'kshitiz', an annual national level sports festival, all organized and conducted by themselves under faculty guidance.",
+    "What are the uniform requirements for B.Tech students?",
+    "B.Tech boys are required to wear a white shirt, steel grey trousers, black shoes with white socks, a plain maroon tie, and a navy blue blazer during winters. Girls wear a steel grey Kurta, white Salwar with a white Dupatta, black shoes/sandals, and a navy blue sweater during winters."
+  ],
+  [
+    "What happens if a student violates the institute rules?",
+    "Students who violate the rules can face punishment depending on the severity of the offense.  This could range from warnings to expulsion from the institute.",
+    "What are some examples of disciplinary offenses?",
+    "Acts of indiscipline include disregarding institute rules, orders, and notices, ignoring staff instructions, noisy or disruptive behavior, ragging, neglecting studies, participating in strikes, unauthorized absences, engaging in violent activities, and forming societies or clubs without permission."
+  ],
+  [
+    "What are the responsibilities of students in terms of equipment and safety?",
+    "Students are responsible for the care of all tools, apparatus, instruments, and materials issued to them and must observe all safety precautions.",
+    "What are students warned about regarding the river?",
+    "Students are strictly prohibited from going to the river for bathing or swimming. Any student caught doing so will face severe punishment."
+  ]
+]
